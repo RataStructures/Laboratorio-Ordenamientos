8cb2160360983ec2eb6789fcc964ee65919fb4ec
Ratabart666 <t.gomezs2@uniandes.edu.co>
2025-07-29 22:33:22 -0500

    Implementación de los algoritmos de ordenamientos

--- DataStructures/List/single_linked_list.py
+++ DataStructures/List/single_linked_list.py
@@ -1,4 +1,5 @@
 from DataStructures.List import list_node as ln
+from random import randint
 
 # Funciones auxiliares
 # Estas funciones NO son parte de la API y NO deben ser utilizadas directamente.
@@ -35,18 +36,42 @@ def decrease_size(my_list):
     return my_list
 
 
+def update_size(my_list, size):
+    my_list["size"] = size
+    return my_list
+
+
+def update_list(my_list, new_list):
+    my_list = update_first(my_list, get_first(new_list))
+    my_list = update_last(my_list, get_last(new_list))
+    my_list = update_size(my_list, size(new_list))
+    return my_list
+
+
+def iterator_node(my_list, start, end, step):
+    first_node = get_first(my_list)
+    current_node = ln.get_node(first_node, start)
+    index = start
+    while current_node is not None and index < end:
+        if index >= start and (index - start) % step == 0:
+            yield current_node
+        index += step
+        current_node = ln.get_node(current_node, step)
+
+
 # Función extra (no documentada en la API) que permite iterar sobre los elementos de la lista
 
 
 def iterator(my_list, start, end, step):
-    current = get_first(my_list)
-    index = 0
-    while current is not None and index < end:
+    first_node = get_first(my_list)
+    current_node = ln.get_node(first_node, start)
+    index = start
+    while current_node is not None and index < end:
         if index >= start and (index - start) % step == 0:
-            result = ln.get_element(current)
-            yield result
-        index += 1
-        current = ln.get_next(current)
+            current_value = ln.get_element(current_node)
+            yield current_value
+        index += step
+        current_node = ln.get_node(current_node, step)
 
 
 # Funciones principales de la API
@@ -270,3 +295,185 @@ def sub_list(my_list, pos, num_elements):
                 break
             index += 1
     return sublist
+
+
+# Funciones auxiliares para algoritmos de ordenamiento iterativos
+# Estas funciones NO son parte de la API y NO deben ser utilizadas directamente.
+# Son funciones auxiliares internas para la implementación de los algoritmos de ordenamiento.
+
+
+def compute_init_gap(n):
+    gap = 1
+    while gap * 3 + 1 <= n // 3:
+        gap = gap * 3 + 1
+    return gap
+
+
+def insertion_sort_with_gap(my_list, start_index, gap, sort_crit):
+    n = size(my_list)
+    need_sort = n > 1
+    if need_sort:
+        for index in range(start_index + gap, n, gap):
+            continue_sorting = True
+            while continue_sorting:
+                current = get_element(my_list, index)
+                previous = get_element(my_list, index - gap)
+                swap = not sort_crit(previous, current)
+                if swap:
+                    my_list = exchange(my_list, index, index - gap)
+                index -= gap
+                continue_sorting = swap and index >= gap
+    return my_list
+
+
+# Algoritmos de ordenamiento iterativos
+def selection_sort(my_list, sort_crit):
+    n = size(my_list)
+    need_sort = n > 1
+    if need_sort:
+        my_sort_list, continue_sorting = new_list(), True
+        while continue_sorting:
+            n, index = size(my_list), -1
+            max, max_index = first_element(my_list), 0
+            for element in iterator(my_list, 0, n, 1):
+                index += 1
+                update = sort_crit(max, element)
+                if update:
+                    max = element
+                    max_index = index
+            my_sort_list = add_last(my_sort_list, max)
+            my_list = delete_element(my_list, max_index)
+            continue_sorting = not is_empty(my_list)
+        my_list = update_list(my_list, my_sort_list)
+    return my_list
+
+
+def insertion_sort(my_list, sort_crit):
+    n = size(my_list)
+    need_sort = n > 1
+    if need_sort:
+        my_sort_list, first_element = new_list(), remove_first(my_list)
+        my_sort_list = add_first(my_sort_list, first_element)
+        continue_sorting = True
+        while continue_sorting:
+            current = remove_first(my_list)
+            index = -1
+            for potential_node in iterator_node(my_sort_list, 0, size(my_sort_list), 1):
+                index += 1
+                potential = ln.get_element(potential_node)
+                is_last = not ln.get_next(potential_node)
+                if sort_crit(current, potential):
+                    my_sort_list = insert_element(my_sort_list, current, index)
+                    break
+                elif is_last:
+                    my_sort_list = add_last(my_sort_list, current)
+            continue_sorting = not is_empty(my_list)
+        my_list = update_list(my_list, my_sort_list)
+    return my_list
+
+
+def shell_sort(my_list, sort_crit):
+    n = size(my_list)
+    need_sort = n > 1
+    if need_sort:
+        gap = compute_init_gap(n)
+        while gap > 0:
+            for start_index in range(gap):
+                my_list = insertion_sort_with_gap(my_list, start_index, gap, sort_crit)
+            gap = (gap - 1) // 3
+    return my_list
+
+
+# Funciones auxiliares para algoritmos de ordenamiento recursivos
+# Estas funciones NO son parte de la API y NO deben ser utilizadas directamente.
+# Son funciones auxiliares internas para la implementación de los algoritmos de ordenamiento recursivos.
+
+
+def merge(left, right, sort_crit):
+    merged = new_list()
+    run, is_empty_left, is_empty_right = True, False, False
+    while run:
+        if is_empty_right:
+            left_element = remove_first(left)
+            merged = add_last(merged, left_element)
+        elif is_empty_left:
+            right_element = remove_first(right)
+            merged = add_last(merged, right_element)
+        else:
+            left_element = first_element(left)
+            right_element = first_element(right)
+            add_left = not sort_crit(right_element, left_element)
+            if add_left:
+                left_element = remove_first(left)
+                merged = add_last(merged, left_element)
+
+            else:
+                right_element = remove_first(right)
+                merged = add_last(merged, right_element)
+        is_empty_right = is_empty(right)
+        is_empty_left = is_empty(left)
+        run = (not is_empty_left) or (not is_empty_right)
+    return merged
+
+
+def partition(my_list, sort_crit):
+    n = size(my_list)
+    pivot = first_element(my_list)
+    less = new_list()
+    greater = new_list()
+    equal = new_list()
+    for element in iterator(my_list, 0, n, 1):
+        is_less = sort_crit(element, pivot)
+        is_greater = sort_crit(pivot, element)
+        if is_less:
+            less = add_last(less, element)
+        elif is_greater:
+            greater = add_last(greater, element)
+        else:
+            equal = add_last(equal, element)
+    return (
+        less,
+        equal,
+        greater,
+    )
+
+
+def combine(less, equal, greater):
+    my_list, n_l, n_e, n_g = new_list(), size(less), size(equal), size(greater)
+    for element in iterator(less, 0, n_l, 1):
+        my_list = add_last(my_list, element)
+    for element in iterator(equal, 0, n_e, 1):
+        my_list = add_last(my_list, element)
+    for element in iterator(greater, 0, n_g, 1):
+        my_list = add_last(my_list, element)
+    return my_list
+
+
+# Algoritmos de ordenamiento recursivos
+
+
+def merge_sort(my_list, sort_crit):
+    n = size(my_list)
+    base_case = n <= 1
+    if not base_case:
+        left = sub_list(my_list, 0, n // 2)
+        right = sub_list(my_list, n // 2, n - n // 2)
+        sorted_left = merge_sort(left, sort_crit)
+        sorted_right = merge_sort(right, sort_crit)
+        my_list = merge(sorted_left, sorted_right, sort_crit)
+    return my_list
+
+
+def quick_sort(my_list, sort_crit):
+    n = size(my_list)
+    base_case = n <= 1
+    if not base_case:
+        (
+            less,
+            equal,
+            greater,
+        ) = partition(my_list, sort_crit)
+        sorted_less = quick_sort(less, sort_crit)
+        sorted_greater = quick_sort(greater, sort_crit)
+        my_list = combine(sorted_less, equal, sorted_greater)
+    return my_list
